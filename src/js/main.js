1eafa87545d3a0b6c3d5868dd0e144a833d729f7
Heraldo Ecard Roque Filho <heraldo663@gmail.com>
2018-12-18 10:56:24 -0200

    added webpack

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,20 @@
+import Teste from "./modules/test";
+
 function ready(fn) {
-    if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading") {
-        fn();
-    } else {
-        document.addEventListener('DOMContentLoaded', fn);
-    }
+  if (
+    document.attachEvent
+      ? document.readyState === "complete"
+      : document.readyState !== "loading"
+  ) {
+    fn();
+  } else {
+    document.addEventListener("DOMContentLoaded", fn);
+  }
 }
 
 function main() {
-    console.log("test");
+  console.log("test");
+  new Teste();
 }
 
 ready(main);
